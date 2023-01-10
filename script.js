976641e3cfd6b35f7c32b5aa1dae20ec9b43b1eb
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-10 17:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+let orderedList = document.getElementById("todoList")
+let list = document.getElementById("newTodoInput")
+
+
+function adding(){
+	if(list.value != ""){
+		let newList = document.createElement("li");
+		newList.innerHTML = list.value
+		orderedList.appendChild(newList)
+		list.value = "";
+	}
+	
+}
 
